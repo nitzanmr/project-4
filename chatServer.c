8bab06407fc7a12d573edc70c25a1516c6b2c7e8
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2023-01-18 15:13:54 +0000

    started working on the write and read functions inside the main function

--- chatServer.c
+++ chatServer.c
@@ -12,123 +12,136 @@ void intHandler(int SIG_INT) {
 	/* use a flag to end_server to break the main loop */
 }
 
-// int main (int argc, char *argv[])
-// {
+int main (int argc, char *argv[])
+{
 	
-// 	signal(SIGINT, intHandler);
-// 	int server_fd = 0;
-// 	conn_pool_t* pool = malloc(sizeof(conn_pool_t));
-// 	init_pool(pool);
-//    	struct sockaddr_in server_socket;
-// 	/*************************************************************/
-// 	/* Create an AF_INET stream socket to receive incoming      */
-// 	/* connections on                                            */
-// 	/*************************************************************/
-// 	if((server_fd = socket(AF_INET,SOCK_STREAM,0))<0){
-// 		printf("Usage: socket failed");
-// 		exit(1);
-// 	}
+	signal(SIGINT, intHandler);
+	int server_fd = 0;
+	conn_pool_t* pool = malloc(sizeof(conn_pool_t));
+	init_pool(pool);
+   	struct sockaddr_in server_socket;
+	char buf[512];
+	int count_read = 0;
+	/*************************************************************/
+	/* Create an AF_INET stream socket to receive incoming      */
+	/* connections on                                            */
+	/*************************************************************/
+	if((server_fd = socket(AF_INET,SOCK_STREAM,0))<0){
+		printf("Usage: socket failed");
+		exit(1);
+	}
    
    
-// 	/*************************************************************/
-// 	/* Set socket to be nonblocking. All of the sockets for      */
-// 	/* the incoming connections will also be nonblocking since   */
-// 	/* they will inherit that state from the listening socket.   */
-// 	/*************************************************************/
-// 	ioctl(...);
-// 	/*************************************************************/
-// 	/* Bind the socket                                           */
-// 	/*************************************************************/
-// 	bind(...);
+	/*************************************************************/
+	/* Set socket to be nonblocking. All of the sockets for      */
+	/* the incoming connections will also be nonblocking since   */
+	/* they will inherit that state from the listening socket.   */
+	/*************************************************************/
+	ioctl(...);
+	/*************************************************************/
+	/* Bind the socket                                           */
+	/*************************************************************/
+	bind(...);
 
-// 	/*************************************************************/
-// 	/* Set the listen back log                                   */
-// 	/*************************************************************/
-// 	listen(...);
+	/*************************************************************/
+	/* Set the listen back log                                   */
+	/*************************************************************/
+	listen(...);
 
-// 	/*************************************************************/
-// 	/* Initialize fd_sets  			                             */
-// 	/*************************************************************/
+	/*************************************************************/
+	/* Initialize fd_sets  			                             */
+	/*************************************************************/
 	
-// 	/*************************************************************/
-// 	/* Loop waiting for incoming connects, for incoming data or  */
-// 	/* to write data, on any of the connected sockets.           */
-// 	/*************************************************************/
-// 	do
-// 	{
-// 		/**********************************************************/
-// 		/* Copy the master fd_set over to the working fd_set.     */
-// 		/**********************************************************/
+	/*************************************************************/
+	/* Loop waiting for incoming connects, for incoming data or  */
+	/* to write data, on any of the connected sockets.           */
+	/*************************************************************/
+	do
+	{
+		/**********************************************************/
+		/* Copy the master fd_set over to the working fd_set.     */
+		/**********************************************************/
 		
-// 		/**********************************************************/
-// 		/* Call select() 										  */
-// 		/**********************************************************/
-// 		select(...);
+		/**********************************************************/
+		/* Call select() 										  */
+		/**********************************************************/
+		select(pool->maxfd,&pool->read_set,&pool->write_set,NULL,10);
 
 		
-// 		/**********************************************************/
-// 		/* One or more descriptors are readable or writable.      */
-// 		/* Need to determine which ones they are.                 */
-// 		/**********************************************************/
+		/**********************************************************/
+		/* One or more descriptors are readable or writable.      */
+		/* Need to determine which ones they are.                 */
+		/**********************************************************/
 		
-// 		for (/* check all descriptors, stop when checked all valid fds */)
-// 		{
-// 			/* Each time a ready descriptor is found, one less has  */
-// 			/* to be looked for.  This is being done so that we     */
-// 			/* can stop looking at the working set once we have     */
-// 			/* found all of the descriptors that were ready         */
+		for (conn_t* cur_conn = pool->conn_head;cur_conn->next != NULL; cur_conn = cur_conn->next/* check all descriptors, stop when checked all valid fds */)
+		{
+			/* Each time a ready descriptor is found, one less has  */
+			/* to be looked for.  This is being done so that we     */
+			/* can stop looking at the working set once we have     */
+			/* found all of the descriptors that were ready         */
 				
-// 			/*******************************************************/
-// 			/* Check to see if this descriptor is ready for read   */
-// 			/*******************************************************/
-// 			if (FD_ISSET(...)
-// 			{
-// 				/***************************************************/
-// 				/* A descriptor was found that was readable		   */
-// 				/* if this is the listening socket, accept one      */
-// 				/* incoming connection that is queued up on the     */
-// 				/*  listening socket before we loop back and call   */
-// 				/* select again. 						            */
-// 				/****************************************************/
-//                 accept(...)
-// 				/****************************************************/
-// 				/* If this is not the listening socket, an 			*/
-// 				/* existing connection must be readable				*/
-// 				/* Receive incoming data his socket             */
-// 				/****************************************************/
-// 				read(...)
-// 				/* If the connection has been closed by client 		*/
-//                 /* remove the connection (remove_conn(...))    		*/
+			/*******************************************************/
+			/* Check to see if this descriptor is ready for read   */
+			/*******************************************************/
+			if (FD_ISSET(cur_conn->fd,&pool->ready_read_set))
+			{
+				/***************************************************/
+				/* A descriptor was found that was readable		   */
+				/* if this is the listening socket, accept one      */
+				/* incoming connection that is queued up on the     */
+				/*  listening socket before we loop back and call   */
+				/* select again. 						            */
+				/****************************************************/
+                if(cur_conn->fd == server_fd){
+					int fd = accept(cur_conn->fd,&server_socket,sizeof(server_socket));
+					add_conn(fd,pool);
+				}
+				/****************************************************/
+				/* If this is not the listening socket, an 			*/
+				/* existing connection must be readable				*/
+				/* Receive incoming data his socket             */
+				/****************************************************/
+				else{
+					while((count_read = read(cur_conn->fd,buf,511)) == 511){
+						add_msg(cur_conn->fd,buf,512,pool);
+					}
+					if(count_read != 0){
+						add_msg(cur_conn->fd,buf,count_read,pool);
+
+					}
+				}
+				/* If the connection has been closed by client 		*/
+                /* remove the connection (remove_conn(...))    		*/
 							
-// 				/**********************************************/
-// 				/* Data was received, add msg to all other    */
-// 				/* connectios					  			  */
-// 				/**********************************************/
-//                 add_msg(...);
+				/**********************************************/
+				/* Data was received, add msg to all other    */
+				/* connectios					  			  */
+				/**********************************************/
+                
 		                  
 				
-// 			} /* End of if (FD_ISSET()) */
-// 			/*******************************************************/
-// 			/* Check to see if this descriptor is ready for write  */
-// 			/*******************************************************/
-// 			if (FD_ISSET(...) {
-// 				/* try to write all msgs in queue to sd */
-// 				write_to_client(...);
-// 		 	}
-// 		 /*******************************************************/
+			} /* End of if (FD_ISSET()) */
+			/*******************************************************/
+			/* Check to see if this descriptor is ready for write  */
+			/*******************************************************/
+			if (FD_ISSET(cur_conn->fd,pool->ready_write_set) {
+				/* try to write all msgs in queue to sd */
+				write_to_client(...);
+		 	}
+		 /*******************************************************/
 		 
 		 
-//       } /* End of loop through selectable descriptors */
+      } /* End of loop through selectable descriptors */
 
-//    } while (end_server == 0);
+   } while (end_server == 0);
 
-// 	/*************************************************************/
-// 	/* If we are here, Control-C was typed,						 */
-// 	/* clean up all open connections					         */
-// 	/*************************************************************/
+	/*************************************************************/
+	/* If we are here, Control-C was typed,						 */
+	/* clean up all open connections					         */
+	/*************************************************************/
 	
-// 	return 0;
-// }
+	return 0;
+}
 
 
 int init_pool(conn_pool_t* pool) {
